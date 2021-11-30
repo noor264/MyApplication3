bc7f2badabe8e1f979af71f3e34ea2f6d895eb6e
Noor Othman <noor.othman264@gmail.com>
2021-11-30 12:42:35 +0200

    3011

--- app/src/main/java/com/example/myapplication/addPlayer.java
+++ app/src/main/java/com/example/myapplication/addPlayer.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    String back="#BFF4DC";
+public class addPlayer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_player);
     }
 }
